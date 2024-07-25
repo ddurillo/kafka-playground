90f0a8148439fe8eb010349e1fea4175fc765de7
David Durillo Moral <d.durillo@teamsystem.com>
2024-07-25 22:57:00 +0200

    Changed import-data

--- src/quickstart/import-data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# https://kafka.apache.org/quickstart#quickstart_kafkaconnect
-COMMAND=("$CONTAINER_KAFKA_FOLDER/bin/connect-standalone.sh" "$CONTAINER_KAFKA_FOLDER/config/connect-standalone.properties" "$CONTAINER_KAFKA_FOLDER/config/connect-file-source.properties")
-
-echo "[EXEC] ${COMMAND[@]}"
-${COMMAND[@]}
