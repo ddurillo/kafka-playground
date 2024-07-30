301f9a262c399726f92b46b5d8cce04bc93868de
David Durillo <ddurillo@teamsystem.com>
2024-07-30 16:43:23 +0200

    Added full write app tutorial

--- Dockerfile
+++ Dockerfile
@@ -4,30 +4,15 @@ ARG CONTAINER_KAFKA_FOLDER
 
 USER root
 
+RUN apk update && apk upgrade
+
 RUN apk add nano
 # Needed for Kafka Streams' WordCount
 RUN apk add libstdc++
-# Needed for Kafka Streams' write app
+# Needed for Kafka Streams' write app (DO NOT CHANGE THE ORDER)
+RUN apk add openjdk8 openjdk8-jre 
+# Do "EXPORT JAVA_HOME=/usr/lib/jvm/java-8-openjdk" once you're in the attached shell
 RUN apk add maven
-# openjdk21 openjdk21-jre
-
-# RUN { \
-# 		echo '#!/bin/sh'; \
-# 		echo 'set -e'; \
-# 		echo; \
-# 		echo 'dirname "$(dirname "$(readlink -f "$(which javac || which java)")")"'; \
-# 	} > /usr/local/bin/docker-java-home \
-# 	&& chmod +x /usr/local/bin/docker-java-home
-# ENV JAVA_HOME /usr/lib/jvm/java-21-openjdk
-# ENV PATH $PATH:/usr/lib/jvm/java-21-openjdk/jre/bin:/usr/lib/jvm/java-21-openjdk/bin
-
-# ENV JAVA_VERSION 8u212
-# ENV JAVA_ALPINE_VERSION 8.212.04-r0
-
-# RUN set -x \
-# 	&& apk add --no-cache \
-# 		openjdk21="$JAVA_ALPINE_VERSION" \
-# 	&& [ "$JAVA_HOME" = "$(docker-java-home)" ]
 
 # For import-data and export-data scripts
 RUN echo "plugin.path=/opt/kafka/libs/connect-file-3.7.1.jar" >> ${CONTAINER_KAFKA_FOLDER}/config/connect-standalone.properties
--- src/kafka-streams/write-app/streams-quickstart/src/main/java/myapps/WordCount.java
+++ src/kafka-streams/write-app/streams-quickstart/src/main/java/myapps/WordCount.java
@@ -17,31 +17,45 @@
 package myapps;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KeyValueMapper;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.ValueMapper;
+import org.apache.kafka.streams.state.KeyValueStore;
 
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * In this example, we implement a simple Pipe program using the high-level Streams DSL
+ * In this example, we implement a simple WordCount program using the high-level Streams DSL
  * that reads from a source topic "streams-plaintext-input", where the values of messages represent lines of text,
- * and writes the messages as-is into a sink topic "streams-pipe-output".
+ * split each text line into words and then compute the word occurrence histogram, write the continuous updated histogram
+ * into a topic "streams-wordcount-output" where each record is an updated count of a single word.
  */
-public class Pipe {
+public class WordCount {
 
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         final StreamsBuilder builder = new StreamsBuilder();
 
-        builder.stream("streams-plaintext-input").to("streams-pipe-output");
+        builder.<String, String>stream("streams-plaintext-input")
+               .flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("\\W+")))
+               .groupBy((key, value) -> value)
+               .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"))
+               .toStream()
+               .to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
 
         final Topology topology = builder.build();
         final KafkaStreams streams = new KafkaStreams(topology, props);
